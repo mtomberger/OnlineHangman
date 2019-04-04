b2d44c429f4df9eec7c5834db5550e0591dd9ded
wechtig <wechtitsch.lukas@gmail.com>
2019-04-04 19:28:55 +0200

    initial commit

---
+++ src/main/java/at/hangman/hangman/HangmanApplication.java
@@ -0,0 +1,13 @@
+package at.hangman.hangman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangmanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HangmanApplication.class, args);
+    }
+
+}
