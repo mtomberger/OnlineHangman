08010165170f4860950b86df4fb14284e118df5e
mtomberger <mtomberger@lukmann.at>
2019-04-12 18:55:50 +0200

    new design check word API

---
+++ src/main/java/at/hangman/hangman/Word.java
@@ -0,0 +1,55 @@
+package at.hangman.hangman;
+
+import java.util.List;
+
+public class Word {
+    private String word;
+    private int score;
+    private List<String> tags;
+    private boolean isNoun = false;
+
+    public Word(String word, int score, List<String> tags) {
+        this.word = word;
+        this.score = score;
+        this.tags = tags;
+        checkForNoun();
+    }
+
+    private void checkForNoun(){
+        this.isNoun = this.tags.stream().anyMatch(m -> m.toLowerCase().equals("n"));
+    }
+
+    public Word() {
+        isNoun=false;
+        checkForNoun();
+    }
+
+    public boolean isNoun() {
+        return isNoun;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+        checkForNoun();
+    }
+}
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,31 +1,24 @@
 'use strict';
 
-var usernamePage = document.querySelector('#username-page');
-var chatPage = document.querySelector('#chat-page');
-var usernameForm = document.querySelector('#usernameForm');
-var messageForm = document.querySelector('#messageForm');
-var messageInput = document.querySelector('#message');
-var messageArea = document.querySelector('#messageArea');
-var connectingElement = document.querySelector('.connecting');
+var usernamePage = $('#username-page');
+var playPage = $('#username-page');
+var usernameForm = $('.gamestart-board');
+var loadingOverlay = $('.loading');
 
 var stompClient = null;
 var username = null;
-
-var colors = [
-    '#2196F3', '#32c787', '#00BCD4', '#ff5652',
-    '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
-];
+var word = null;
 
 function connect(event) {
-    username = document.querySelector('#name').value.trim();
-
-    if(username) {
-        usernamePage.classList.add('hidden');
-        chatPage.classList.remove('hidden');
+    username =$('#name').val().trim();
+    word  = $('#word').val().trim();
+    if(username && word) {
+        usernamePage.addClass('hidden');
+        playPage.removeClass('hidden');
 
         var socket = new SockJS('/ws');
         stompClient = Stomp.over(socket);
-
+        loadingOverlay.removeClass('hidden');
         stompClient.connect({}, onConnected, onError);
     }
     event.preventDefault();
@@ -39,16 +32,14 @@ function onConnected() {
     // Tell your username to the server
     stompClient.send("/app/chat.addUser",
         {},
-        JSON.stringify({sender: username, type: 'JOIN'})
+        JSON.stringify({sender: username, content: word, type: 'JOIN'})
     )
-
-    connectingElement.classList.add('hidden');
+    loadingOverlay.addClass('hidden');
 }
 
 
 function onError(error) {
-    connectingElement.textContent = 'Could not connect to WebSocket server. Please refresh this page to try again!';
-    connectingElement.style.color = 'red';
+
 }
 
 
@@ -76,53 +67,7 @@ function onMessageReceived(payload) {
         messageElement.classList.add('event-message');
         message.content = message.sender + ' joined!';
         appendText(message, messageElement);
-    } else if (message.type === 'LEAVE') {
-        messageElement.classList.add('event-message');
-        message.content = message.sender + ' left!';
-        appendText(message, messageElement);
-    } else {
-        messageElement.classList.add('chat-message');
-
-        var avatarElement = document.createElement('i');
-        var avatarText = document.createTextNode(message.sender[0]);
-        avatarElement.appendChild(avatarText);
-        avatarElement.style['background-color'] = getAvatarColor(message.sender);
-
-        messageElement.appendChild(avatarElement);
-
-        var usernameElement = document.createElement('span');
-        var usernameText = document.createTextNode(message.sender);
-        usernameElement.appendChild(usernameText);
-        messageElement.appendChild(usernameElement);
-        var messageText = document.createTextNode(message.content);
-
-        var receiver = messageText.data.split(":")[1];
-        if(username == receiver || message.sender == username) {
-            appendText(message, messageElement);
-        }
     }
+
 }
-
-function appendText(message, messageElement) {
-    var textElement = document.createElement('p');
-    var messageText = document.createTextNode(message.content);
-    textElement.appendChild(messageText);
-
-    messageElement.appendChild(textElement);
-
-    messageArea.appendChild(messageElement);
-    messageArea.scrollTop = messageArea.scrollHeight;
-}
-
-
-function getAvatarColor(messageSender) {
-    var hash = 0;
-    for (var i = 0; i < messageSender.length; i++) {
-        hash = 31 * hash + messageSender.charCodeAt(i);
-    }
-    var index = Math.abs(hash % colors.length);
-    return colors[index];
-}
-
-usernameForm.addEventListener('submit', connect, true)
-messageForm.addEventListener('submit', sendMessage, true)
+usernameForm.on('submit', connect);
